948b4d8b1f2d65c9a449d9e3e40edb821dc481a1
Thomas Lindeker <tlindeker0@gmail.com>
2023-10-30 11:40:21 +0100

    initial commit

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.title = "My Game"          
+    t.version = "11.3"           
+    t.window.fullscreen = true  -- mettre en plein écran
+    t.window.vsync = 1           
+    t.window.msaa = 0            
+    t.console = false
+end
